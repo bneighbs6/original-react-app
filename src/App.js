ac6d097befea2d618cb92551f8b0309a95b5cae4
Brett <brett_neighbors@yahoo.com>
2024-01-08 20:53:38 -0700

    Editted app.js to return a button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
 
+const handleClick = (e) => {
+  console.log(e.target);
+}
+
 function App() {
-  return <h1>Hello World!</h1>>
+  return (
+    <div>  
+    <h1>Hello Shalaina!</h1>
+    <button onClick={handleClick}>Subscribe</button>
+    </div>
+  )
 }
 
 export default App;
